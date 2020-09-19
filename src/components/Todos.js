1d390954587b70af7695bb19f2dd838a6073979e
nasthosh <santhosh.karthik@outlook.com>
2020-09-19 14:28:27 +1000

    Created Text Box and Drop Down

---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todos = () => {
+    return(
+        <div className="todo-container">
+            <ul className="todo-list"></ul>
+        </div>
+
+    );
+};
+
+export default Todos;

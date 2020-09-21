2e8ca6d261a3399f720c1f51d2d2dc32420a2529
nasthosh <santhosh.karthik@outlook.com>
2020-09-21 11:44:06 +1000

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -25,11 +25,9 @@ function App() {
                 break;
             case 'not completed':
                 setFilteredTodos(todos.filter(todo => todo.completed === false));
-                //countTotalTasks = todos.length;
                 break;
             default:
                 setFilteredTodos(todos);
-                //countTotalTasks = todos.length;
                 break;
         }
   };

a542c326254dc4ec1031ab0204cf2b90bef8a8f7
nasthosh <santhosh.karthik@outlook.com>
2020-10-14 11:33:14 +1100

    Small CHnages

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,56 @@
 import React from "react";
 
+const Form = ({
+  setInputValue,
+  todos,
+  setTodos,
+  inputValue,
+  setToggleStatus,
+}) => {
+  const inputHandler = (e) => {
+    console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
 
-const Form = ({setInputValue, todos, setTodos, inputValue, setToggleStatus}) => {
-    const inputHandler = (e) => {
-        console.log(e.target.value);
-        setInputValue(e.target.value);
-    };
+  const submitHandler = (e) => {
+    console.log(e.target.value);
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputValue, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputValue("");
+  };
 
-    const submitHandler = (e) => {
-        console.log(e.target.value);
-        e.preventDefault();
-        setTodos([
-            ...todos,{text : inputValue, completed : false, id : Math.random() * 1000}
-        ])
-        setInputValue("");
-    };
+  const toggleStatusHandler = (e) => {
+    console.log(e.target.value);
+    setToggleStatus(e.target.value);
+  };
 
-    const toggleStatusHandler = (e) => {
-        console.log(e.target.value);
-        setToggleStatus(e.target.value);
-    };
-
-    return(
-        <form>
-            <input value={inputValue} onChange={inputHandler} type="text" className="todo-input"/>
-            <button onClick={submitHandler} className="todo-button" type="submit">
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <div className="select">
-                <select onChange = {toggleStatusHandler} name="todos" className="filter-todo">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="not completed">Not Completed</option>
-                </select>
-            </div>
-        </form>
-    );
+  return (
+    <form>
+      <input
+        value={inputValue}
+        onChange={inputHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select
+          onChange={toggleStatusHandler}
+          name="todos"
+          className="filter-todo"
+        >
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="not completed">Not Completed</option>
+        </select>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
